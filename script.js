b002f48fae34d1e2789d7ec2c3a3b724505f1789
uts1mask-solomon <uts1mask@gmail.com>
2026-07-25 04:06:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 const title = document.getElementById("title");
 title.textContent = "World History Quiz";
 
-fetch("questions.json")
-  .then(response => response.json())
-  .then(data => {
-      console.log(data);
-      console.log(data[0].question);
-  });
-  
-  
+
